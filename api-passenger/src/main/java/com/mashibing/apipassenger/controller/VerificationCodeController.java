8ec2ee22e205c4be70f7ba97cb05d836661933c3
Forever <forever@ForeverdeMacBook-Pro.local>
2025-08-08 17:26:12 +0800

    修改service

--- api-passenger/src/main/java/com/mashibing/apipassenger/controller/VerificationCodeController.java
+++ api-passenger/src/main/java/com/mashibing/apipassenger/controller/VerificationCodeController.java
@@ -1,6 +1,7 @@
 package com.mashibing.apipassenger.controller;
 
 import com.mashibing.apipassenger.request.VerificationCodeDTO;
+
 import com.mashibing.apipassenger.service.VerificationCodeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
--- api-passenger/src/main/java/com/mashibing/apipassenger/service/VerificationCodeService.java
+++ api-passenger/src/main/java/com/mashibing/apipassenger/service/VerificationCodeService.java
@@ -1,24 +1,6 @@
 package com.mashibing.apipassenger.service;
 
-import net.sf.json.JSONObject;
-import org.springframework.stereotype.Service;
+public interface VerificationCodeService {
 
-@Service
-public class VerificationCodeService {
-
-   public String generatorCode(String passengerPhone){
-        //调用验证码服务,获取验证码
-        String code = "1111";
-        System.out.println("调用验证码服务,获取验证码");
-       System.out.println(passengerPhone);
-        //存入redis
-        System.out.println("存入redis");
-
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("code",1);
-        jsonObject.put("message","success");
-        return jsonObject.toString();
-    }
-
-
+    String generatorCode(String passengerPhone);
 }
